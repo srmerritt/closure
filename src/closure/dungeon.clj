fb3859a22c182aee3215fe090a6ebdf109f5dac5
Jon Bender <jon.bender@rd.io>
2015-12-08 11:07:45 -0800

    Make this a bit faster

--- src/closure/dungeon.clj
+++ src/closure/dungeon.clj
@@ -1,25 +1,23 @@
 (ns closure.dungeon
   (:require [closure.cell :as cell]))
 
-(defn- get-adjacencies [row col]
+(defn- get-adjacencies [[row col]]
   [[(inc row) col] [(dec row) col]
    [row (inc col)] [row (dec col)]])
 
 (defn- get-next-cell [dungeon filled-indices]
-  (let 
-    [available-cells (set
-                       (apply concat
-                        (for [[row col] filled-indices
-                              :let [adjacencies (get-adjacencies row col)]]
-                          (filter #(= :oob (get-in dungeon %)) adjacencies))))]
-     (rand-nth (seq available-cells))))
-
+  (loop [[index & indices] (shuffle filled-indices)]
+    (let [is-oob? (fn [idx] (= :oob (get-in dungeon idx)))
+          valid-adjacencies (filter is-oob? (get-adjacencies index))]
+      (if (seq valid-adjacencies) (rand-nth valid-adjacencies)
+        (recur indices)))))
+      
 (defn generate-dungeon [height width]
   (let 
     [empty-dungeon (vec (repeat height (vec (repeat width :oob))))
-     seed [(rand-nth (range width)) (rand-nth (range height))]
-     starting-dungeon (assoc-in empty-dungeon seed (cell/new))]
-    (loop [dungeon starting-dungeon
+     seed [(rand-nth (range height)) (rand-nth (range width))]
+     seeded-dungeon (assoc-in empty-dungeon seed (cell/new))]
+    (loop [dungeon seeded-dungeon
            filled #{seed}]
       (if (> (count filled) (* 0.75 height width))
         [filled dungeon]
